b73786fa2e022242b3c1d4793804070337136b28
aurianebgnl <auriane.bgnl@gmail.com>
2025-03-28 11:37:28 +0100

    étape 4 - erreur ville non trouvée

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// A toi de jouer pour cette partie :-) Happy coding !
-
 const BTN_OK = document.querySelector("#btn-ok");
 const CITY_OUTPUT = document.querySelector("#city");
 const GPS_OUTPUT = document.querySelector("#gps");
 const TEMP_OUTPUT = document.querySelector("#temperature");
 const DETAILS_OUTPUT = document.querySelector("#details");
-
-
-
-
-
 
 // fonction pour récupérer le nom de la ville à rechercher
 function getCityName() {
@@ -23,9 +16,19 @@ function showOutput(name, latitude, longitude, temp) {
     CITY_OUTPUT.innerHTML = name
     GPS_OUTPUT.innerHTML = `Coordonées GPS : ${latitude}, ${longitude}`
     TEMP_OUTPUT.innerHTML = `${temp}°C`
+    DETAILS_OUTPUT.innerHTML = "Température actuelle";
 
     return
+}
 
+// Fonction affichage résultat en cas d'erreur
+function showOutputError(){
+    CITY_OUTPUT.innerHTML = 'Ville non trouvée';
+    GPS_OUTPUT.innerHTML = `_`
+    TEMP_OUTPUT.innerHTML = `_`
+    DETAILS_OUTPUT.innerHTML = "Vérifiez le nom de la ville";
+
+    return
 }
 
 // Fonction pour récuperer les coordonnées GPS d'une ville sur l'API Open Street Map
@@ -34,9 +37,9 @@ async function fetchCoordinates(city) {
   
       const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${city}&format=json&addressdetails=1&limit=1`);
       const data = await response.json();
-  
+      
       console.log(data);
-      return data;
+      return data 
   
     } catch (error) {
       console.error("Erreur lors de la récupération des coordonnées GPS :", error);
@@ -64,17 +67,19 @@ async function fetchWeather(cityLat, cityLon) {
 BTN_OK.addEventListener("click", async () => {
     let cityInput = document.querySelector("#city-input").value;
     const city = await fetchCoordinates(cityInput);
+    console.log(city);
+
+    if (city.length === 0) {
+        showOutputError()
+        return
+    }
+
     const cityName = city[0].name
     const cityLat = city[0].lat
     const cityLon = city[0].lon
+    
     const temp = await fetchWeather(cityLat, cityLon)
     const cityTemp = temp.current.temperature_2m
-
-    console.log(city);
-
-    // Afficher les coordonnées GPS dans la console
-    console.log("Latitude :", city[0].lat);
-    console.log("Longitude :", city[0].lon);
 
     showOutput(cityName, cityLat, cityLon, cityTemp)
 
